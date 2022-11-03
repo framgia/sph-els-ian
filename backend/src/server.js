a7ae23d4504316bc4f619b8f541ef6c5c94c2d2d
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2022-11-03 13:18:40 +0800

    conflict fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
 //Packages
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 const cors = require("cors");
 
 //Local Variables/Packages
 const port = process.env.PORT || 5000;
-const db = require("./models");
 const { errorHandler } = require("./middleware/errorMiddleware");
 
 //Options
@@ -15,7 +15,7 @@ var corsOptions = {
 };
 
 //Package Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 

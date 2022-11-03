9da1c15f58fe8b6992b2c3b9f8cdf22ff08ff969
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2022-11-03 10:37:45 +0800

    cleaned up authMiddleware

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -7,17 +7,11 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
-      //get token from header
+      //Get token from header
       const token = req.headers.authorization.split(" ")[1];
-
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      //Get user from the token
-      // req.user = await //find decoded.id minus the password
-
       next();
     } catch (error) {
-      console.log(error);
       res.status(401);
       throw new Error("Not Authorized");
     }

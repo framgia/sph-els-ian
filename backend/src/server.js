cef24cac47c6a5d6b292917294a05a3243330b1d
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2022-11-03 16:31:54 +0800

    fixed corsOptions

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 
 //Options
 var corsOptions = {
-  origin: "http:localhost:3000",
+  origin: "http://localhost:3000",
 };
 
 //Package Middleware

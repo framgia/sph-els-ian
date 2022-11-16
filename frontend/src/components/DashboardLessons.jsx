1957cdfad3723f3f615ba20938d9076766fdf47e
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2022-11-16 17:42:24 +0800

    [SELS-Feature][FE] Dashboard Page
    removed console.log and comments
    
    removed console.log
    
    Added catcher for empty progress
    
    removed comments

---
+++ frontend/src/components/DashboardLessons.jsx
@@ -0,0 +1,5 @@
+import { connect, useDispatch } from "react-redux";
+const DashboardLessons = () => {
+  return <div className="DashboardLessons">Lessons</div>;
+};
+export default connect(null, null)(DashboardLessons);
---
+++ frontend/src/components/DashboardWords.jsx
@@ -0,0 +1,5 @@
+import { connect, useDispatch } from "react-redux";
+const DashboardWords = () => {
+  return <div className="DashboardWords">words</div>;
+};
+export default connect(null, null)(DashboardWords);
